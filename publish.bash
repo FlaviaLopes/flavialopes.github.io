80c1a8f83e4999ea206eda328af12061d98e8691
Flávia <flavithalopes@hotmail.com>
2020-04-16 00:16:52 -0300

    Adiciona script Google Analytics
    Tag de acompanhamento de tráfego em head.

---
+++ publish.bash
@@ -0,0 +1,11 @@
+now="$(date +'%d/%m/%Y')"
+
+pelican -o deploy -s publishconf.py
+
+ghp-import deploy -b gh-pages -m "Publicado em $now"
+
+git push git@github.com:FlaviaLopes/flavialopes.github.io.git gh-pages:master
+
+
+
+
