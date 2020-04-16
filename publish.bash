3a6fca3dadb788c171f09ded7350fab171e0006c
Flávia <flavithalopes@hotmail.com>
2020-04-16 00:31:11 -0300

    Adiciona script Google Analytics
    tag de acompanhamento em head

--- publish.bash
+++ publish.bash
@@ -2,7 +2,7 @@ now="$(date +'%d/%m/%Y')"
 
 pelican -o deploy -s publishconf.py
 
-ghp-import deploy -b gh-pages -m "Publicado em $now" --cname="flavialopes.dev"
+ghp-import deploy -b gh-pages -m "Publicado em $now"
 
 git push git@github.com:FlaviaLopes/flavialopes.github.io.git gh-pages:master
 
